1c491b2de789c9b607ae149d9759e3a731988f12
spookiebruh <zackeboibeat@gmail.com>
2023-11-07 10:38:24 +0100

    Dicerolls and wincheck done, max 5k money working

--- diceGame/diceGame.cpp
+++ diceGame/diceGame.cpp
@@ -8,10 +8,14 @@ using namespace std;
 
 int main()
 {
-    int dice1;
-    int dice2;
+    int dicePlayer1;
+    int diceCpu1;
+    int dicePlayer2;
+    int diceCpu2;
     int money;
     int age;
+
+    srand(time(0));
 
     cout << "Welcome to Dice Game!" << endl;
     cout << "How old are you?" << endl;
@@ -22,11 +26,37 @@ int main()
         return 0;
     }
 
-    srand(time(0));
-    dice1 = rand() % 6 + 1;
-
-    cout << dice1;
+    while (true)
+    {
+        cout << "How much money do you want to insert?" << endl;
+        cin >> money;
+        if (money > 5000)
+        {
+            cout << "Don't insert more than $5000, it's bad for your personal economy. Insert money." << endl;
+            cin >> money;
+        }
+        break;
+    }
     
+    cout << "$" << money << endl;
+    
+
+    dicePlayer1 = rand() % 6 + 1;
+    dicePlayer2 = rand() % 6 + 1;
+    
+    diceCpu1 = rand() % 6 + 1;
+    diceCpu2 = rand() % 6 + 1;
+    cout << "You got " << dicePlayer1 + dicePlayer2 << endl;
+    cout << "Computer got " << diceCpu1 + dicePlayer2 << endl;
+
+    if (dicePlayer1+dicePlayer2 > diceCpu1+diceCpu2)
+    {
+        cout << "You won!";
+    }
+    else
+    {
+        cout << "You lost.";
+    }
 }
 
 
